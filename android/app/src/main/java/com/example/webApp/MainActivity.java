659394294387446967d4db2715020d5b429f3173
StarOfGitHub <sparklingstar08888@gmail.com>
2023-11-13 12:08:46 +0200

    first commit

---
+++ android/app/src/main/java/com/example/webApp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.example.webApp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+   constructor(private http: HttpClient) {}
+
+  sendPushNotification() {
+    this.http.get('http://localhost:3000').subscribe((response) => {
+      console.log(response);
+    });
+  }
   title = 'app';
 }
